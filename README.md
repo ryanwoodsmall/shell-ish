ab8d655e6a69c22a211782ba236c46f86d5a7f40
ryan woodsmall <rwoodsmall@gmail.com>
2014-12-20 01:19:42 -0600

    moving stuff around HTTPS cert grabber

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 shell-ish
 =========
 
-shell (Bash 3/4, Busybox ash both verified) snippets that are, uh, something
+shell (Bash and Busybox ash both verified) snippets that are, uh, something
+
+- lib: library functions that can be dropped into profile, shell rc files and/or sourced
+- bin: scripts
---
+++ bin/gethttpscert.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env sh
+
+usage () {
+	cat <<-USAGE
+	$@ -h <host> -p <port>
+	  -h : hostname/IP address
+	  -p : port number
+	USAGE
+}
+
+progname=`basename $0`
+
+if [ $# -ne 4 ] ; then 
+	usage "$progname"
+	exit 1
+fi
+
+while getopts "h:p:" opt ; do
+	case $opt in
+	h)
+		HTTPSHOST="$OPTARG"
+		;;
+	p)
+		HTTPSPORT="$OPTARG"
+		;;
+	\?)
+		usage "$progname"
+		exit 1
+		;;
+	esac
+done
+
+echo -e 'HEAD / HTTP/1.0\r\n\r\n' | \
+  openssl \
+    s_client \
+    -connect $HTTPSHOST:$HTTPSPORT 2>/dev/null | \
+      sed '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/!d'
