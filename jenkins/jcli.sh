4102892652ae50fa7056f6460bd09a6f3815b5bf
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-08 19:53:27 +0000

    add jenkins context var; check that downloaded jar is valid zip before trying to run it

--- jenkins/jcli.sh
+++ jenkins/jcli.sh
@@ -8,10 +8,11 @@
 #  jo - options to pass to the cli, default empty
 #  js - scheme, either http or https, default http
 #  jh - jenkins master hostname or ip, default localhost
+#  jc - jenkins context including slash (path, ala /jenkins), default empty
 #  jp - jenkins master port, default 8080
 #
 
-for prereq in curl java ; do
+for prereq in curl java unzip ; do
   which ${prereq} >/dev/null 2>&1 || {
     echo "${prereq} not found"
     exit 1
@@ -23,7 +24,10 @@ wdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 : ${js:="http://"}
 : ${jh:="localhost"}
 : ${jp:="8080"}
-ju="${js}${jh}:${jp}"
+: ${jc:=""}
+# repeated slashes (//) will be squashed to a single slash
+ju="${jh}:${jp}/${jc}"
+ju="${js}${ju//\/\///}"
 jcj="jenkins-cli.jar"
 jcju="${ju}/jnlpJars/${jcj}"
 copts="-k -L -s"
@@ -34,6 +38,10 @@ export JENKINS_URL="${ju}"
 
 test -e "${wdir}/${jcj}" || {
   curl ${copts} -o "${wdir}/${jcj}" "${jcju}"
+}
+unzip -l "${wdir}/${jcj}" >/dev/null 2>&1 || {
+  echo "${wdir}/${jcj} doesn't appear to be a valid zip file"
+  exit 1
 }
 
 eval "${jcli}" "${@}"
