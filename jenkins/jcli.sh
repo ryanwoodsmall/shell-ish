5f8eccc15a8309da6e002d218c98e0cbb510f657
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-08 16:20:24 +0000

    reorder vars; unquote curl opts

--- jenkins/jcli.sh
+++ jenkins/jcli.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 wdir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+: ${js:="http://"}
 : ${jh:="jm"}
 : ${jp:="8080"}
-: ${js:="http://"}
 ju="${js}${jh}:${jp}"
 jcj="jenkins-cli.jar"
 jcju="${ju}/jnlpJars/${jcj}"
@@ -11,7 +11,7 @@ copts="-k -L -s"
 jcli="java -jar ${wdir}/${jcj} -s ${ju}"
 
 test -e "${wdir}/${jcj}" || {
-	curl "${copts}" -o "${wdir}/${jcj}" "${jcju}"
+	curl ${copts} -o "${wdir}/${jcj}" "${jcju}"
 }
 
 eval "${jcli}" "${@}"
