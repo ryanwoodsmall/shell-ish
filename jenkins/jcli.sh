0322c0a5c5c70b018402984ccd99f0d3a16f9525
ryan woodsmall <rwoodsmall@gmail.com>
2018-02-08 16:22:40 +0000

    add empty jopts environment var to jcli.sh jenkins cli script

--- jenkins/jcli.sh
+++ jenkins/jcli.sh
@@ -8,7 +8,7 @@ ju="${js}${jh}:${jp}"
 jcj="jenkins-cli.jar"
 jcju="${ju}/jnlpJars/${jcj}"
 copts="-k -L -s"
-jcli="java -jar ${wdir}/${jcj} -s ${ju}"
+jcli="java -jar ${wdir}/${jcj} -s ${ju} ${jopts}"
 
 test -e "${wdir}/${jcj}" || {
 	curl ${copts} -o "${wdir}/${jcj}" "${jcju}"
