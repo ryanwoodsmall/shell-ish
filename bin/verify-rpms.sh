531957fdf1a3fe33e485ac138b6602ed894418ad
ryan woodsmall <rwoodsmall@gmail.com>
2018-04-05 02:02:18 +0000

    add verify-rpms.sh script

---
+++ bin/verify-rpms.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# verify contents of rpm
+#
+# get list of files with package with something like:
+#
+#  grep -B1 / /tmp/rpmverify.out
+#
+
+if [ ${UID} -ne 0 ] ; then
+  echo 'please run as root' >&2
+  exit 1
+fi
+
+rpm -qa | sort | while read -r p ; do
+  echo $p >&2
+  echo $p
+  rpm -qV $p
+  echo
+done > /tmp/rpmverify.out
