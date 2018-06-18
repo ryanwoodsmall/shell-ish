a5ebfc75a16b1e71d02391f225ca62a8730b77ae
ryan woodsmall <rwoodsmall@gmail.com>
2018-06-18 05:03:57 +0000

    add coreutils single-binary lister, use eval since getting its list is ugly

--- bin/box-utils.sh
+++ bin/box-utils.sh
@@ -3,20 +3,23 @@
 #
 # print count and provider for all *box based utils
 #  b : busybox
+#  c : coreutils
 #  s : sbase-box
 #  t : toybox
 #  u : ubase-box
 #
 
 declare -A uaa
-for u in busybox sbase-box toybox ubase-box ; do
+for u in busybox coreutils sbase-box toybox ubase-box ; do
   p="${u:0:1}"
   a=''
   if [[ $u =~ busybox ]] ; then
     a='--list'
+  elif [[ $u =~ coreutils ]] ; then
+    a='--help 2>&1 | sed -n "/^Built-in programs:/,/^$/p" | grep -v ^Built-in | xargs echo'
   fi
   if $(which ${u} >/dev/null 2>&1) ; then
-    for t in $(${u} ${a}) ; do
+    for t in $(eval "${u} ${a}") ; do
       uaa["${t}"]+="${p}"
     done
   fi
