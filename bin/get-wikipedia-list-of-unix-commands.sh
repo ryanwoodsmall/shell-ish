e805578db4e72da6f0d822cc0e58341ef223a960
ryan woodsmall <rwoodsmall@gmail.com>
2018-06-22 02:57:47 +0000

    remove fixed unknown stuff: sccs (cssc) bits, obsolete batch programs, shell builtins, c/c++ programming stuff

--- bin/get-wikipedia-list-of-unix-commands.sh
+++ bin/get-wikipedia-list-of-unix-commands.sh
@@ -17,44 +17,15 @@ curl -kLs https://en.wikipedia.org/wiki/List_of_Unix_commands \
 
 # XXX - figure these out not found stuff:
 #
-# admin - sccs - opt
 # asa - text - opt
 # at - proc_mgmt - mand
 # batch - proc_mgmt - mand
-# c99 - c_prog - opt (gcc)
-# cflow - c_prog - opt
-# ctags - c_prog - opt
-# cxref - c_prog - opt
-# delta - sccs - opt
-# fort77 - fort_prog - opt
-# get - sccs - opt
-# hash - db - mand (bash builtin)
-# lex - c_prog - opt (flex)
+# fort77 - fort_prog - opt (gcc/g77)
 # lp - text_proc - mand
-# m4 - misc - mand (gnu m4)
 # mailx - misc - mand
 # pax - misc - mand
-# prs - sccs - opt
-# qalter - batch - obs
-# qdel - batch - obs
-# qhold - batch - obs
-# qmove - batch - obs
-# qmsg - batch - obs
-# qrerun - batch - obs
-# qrls - batch - obs
-# qselect - batch - obs
-# qsig - batch - obs
-# qstat - batch - obs
-# qsub - batch - obs
-# rmdel - sccs - opt
-# sact - sccs - opt
-# sccs - sccs - opt
 # talk - misc - opt
-# type - misc - opt
-# unget - sccs - opt
 # uucp - net - opt
 # uustat - net - opt
 # uux - proc_mgmt - opt
-# val - sccs - opt
-# what - sccs - opt
 #
