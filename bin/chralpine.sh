bb0cfa582c570a7623c92fbb8f168ec708948ac2
ryan woodsmall <rwoodsmall@gmail.com>
2018-06-28 19:11:24 +0000

    chralpine.sh : update default alpine miniroot version to 3.8.0

--- bin/chralpine.sh
+++ bin/chralpine.sh
@@ -57,7 +57,7 @@ function scriptecho() {
 
 # default versions/names/arch
 chrname="alpine"
-alpver="3.7.0"
+alpver="3.8.0"
 alparch="$(uname -m | sed 's/^\(arm\).*/\1hf/g;s/^i.86$/x86/g')"
 sshport="22222"
 
