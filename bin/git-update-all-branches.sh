0bb6c7bc3ccd1dd1b05e160c2ae232a8b7cc932a
ryan woodsmall <rwoodsmall@gmail.com>
2019-02-05 20:32:11 +0000

    git-update-all-branches.sh: add script

---
+++ bin/git-update-all-branches.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ob="$(git branch -l | grep '^\*' | awk '{print $NF}')"
+for b in $(git branch -l -a | awk -F/ '/remotes\/origin\//{print $NF}' | sort -u) ; do
+	git checkout $b
+	git pull
+done
+git checkout "${ob}"
