344fdfe90e328cc1215a1904cbc74572d42c3149
ryan woodsmall <rwoodsmall@gmail.com>
2019-02-05 20:34:16 +0000

    git-update-all-branches.sh: branch var quoting

--- bin/git-update-all-branches.sh
+++ bin/git-update-all-branches.sh
@@ -2,7 +2,7 @@
 
 ob="$(git branch -l | grep '^\*' | awk '{print $NF}')"
 for b in $(git branch -l -a | awk -F/ '/remotes\/origin\//{print $NF}' | sort -u) ; do
-	git checkout $b
+	git checkout "${b}"
 	git pull
 done
 git checkout "${ob}"
