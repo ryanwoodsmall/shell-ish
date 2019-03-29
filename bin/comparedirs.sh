c5a9392393ad91ba91588d29755ba82d2ec5b8a8
ryanwoodsmall <rwoodsmall@gmail.com>
2019-03-29 02:27:38 +0000

    comparedirs.sh: make output look a bit like "diff --brief --recurisve d1/ d2/"

--- bin/comparedirs.sh
+++ bin/comparedirs.sh
@@ -2,6 +2,8 @@
 
 #
 # quick and dirty "compare all files in two directories" thing
+# gnu diff can do this with "diff --brief --recursive d1/ d2/"
+# we sort of mimic its output
 #
 
 set -eu
@@ -40,20 +42,23 @@ done
 
 for d1file in ${d1files[@]} ; do
   if [ -z "${d2sha256[${d1file}]}" ] ; then
-    echo "${d2}/${d1file}: no such file or directory"
+    echo "Only in ${d1}: ${d1file}"
     continue
-  fi
-  if [ "${d1sha256[${d1file}]}" != "${d2sha256[${d1file}]}" ] ; then
-    echo "${d1}/${d1file}: differs from ${d2}/${d1file}"
   fi
 done | tr -s '/'
 
 for d2file in ${d2files[@]} ; do
   if [ -z "${d1sha256[${d2file}]}" ] ; then
-    echo "${d1}/${d2file}: no such file or directory"
+    echo "Only in ${d2}: ${d2file}"
     continue
   fi
-  if [ "${d2sha256[${d2file}]}" != "${d1sha256[${d2file}]}" ] ; then
-    echo "${d2}/${d2file}: differs from ${d1}/${d2file}"
+done | tr -s '/'
+
+for d1file in ${d1files[@]} ; do
+  if [ -z "${d2sha256[${d1file}]}" ] ; then
+    continue
+  fi
+  if [ "${d1sha256[${d1file}]}" != "${d2sha256[${d1file}]}" ] ; then
+    echo "Files ${d1}/${d1file} and ${d2}/${d1file} differ"
   fi
 done | tr -s '/'
