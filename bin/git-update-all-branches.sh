cda571c0644680cac14fa78ed080f0a227aa4356
ryan woodsmall <rwoodsmall@gmail.com>
2019-04-17 14:51:36 +0000

    git-update-all-branches.sh: test for .git directory; run "git fetch -p" to prune remote branches

--- bin/git-update-all-branches.sh
+++ bin/git-update-all-branches.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ ! -e ./.git/ ] ; then
+  echo "no .git directory found"
+  exit 1
+fi
+git fetch -p
 ob="$(git branch -l | grep '^\*' | awk '{print $NF}')"
 for b in $(git branch -l -a | awk -F/ '/remotes\/origin\//{print $NF}' | sort -u) ; do
-	git checkout "${b}"
-	git pull
+  git checkout "${b}"
+  git pull
 done
 git checkout "${ob}"
