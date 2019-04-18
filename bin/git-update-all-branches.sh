bfd6680e6374bcb7e3c349fa05c9c1cd9efe0b6a
ryan woodsmall <rwoodsmall@gmail.com>
2019-04-18 14:35:45 +0000

    git-update-all-branches.sh: disable .git check, oof

--- bin/git-update-all-branches.sh
+++ bin/git-update-all-branches.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ ! -e ./.git/ ] ; then
-  echo "no .git directory found"
-  exit 1
-fi
+#if [ ! -e ./.git/ ] ; then
+#  echo "no .git directory found"
+#  exit 1
+#fi
 git fetch -p
 ob="$(git branch -l | grep '^\*' | awk '{print $NF}')"
 for b in $(git branch -l -a | awk -F/ '/remotes\/origin\//{print $NF}' | sort -u) ; do
