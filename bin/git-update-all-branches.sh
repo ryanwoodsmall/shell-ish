5e923fe0d619898a42e0dfc36d89af35781ec396
ryanwoodsmall <rwoodsmall@gmail.com>
2019-05-01 15:56:48 +0000

    git-update-all-branches.sh: be a little smarter about branch names

--- bin/git-update-all-branches.sh
+++ bin/git-update-all-branches.sh
@@ -1,13 +1,46 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# XXX - need to verify detached HEAD, etc.
+# exit early/often
+#set -eu
+
+# XXX - can't do this from a subdir
 #if [ ! -e ./.git/ ] ; then
 #  echo "no .git directory found"
 #  exit 1
 #fi
-git fetch -p
+
+# current branch
 ob="$(git branch -l | grep '^\*' | awk '{print $NF}')"
-for b in $(git branch -l -a | awk -F/ '/remotes\/origin\//{print $NF}' | sort -u) ; do
+# list of remotes we can strip
+remotes=( $(git remote -v | awk '{print $1}' | sort -u) )
+# list of all branches
+branches=( $(git branch -la | awk '{print $NF}') )
+# associative array to indicate unique branch names
+declare -A ub
+
+# clean up old/non-existent remotes
+git fetch -p
+
+# loop through *all* branches
+for i in ${!branches[@]} ; do
+  # rip off remote/.../
+  for r in ${remotes[@]} ; do
+    branches[${i}]="${branches[${i}]#remotes/${r}/}"
+  done
+  # rip off origin/
+  if [[ ${branches[${i}]} =~ origin/ ]] ; then
+    branches[${i}]="${branches[${i}]#origin/}"
+  fi
+  # indicate unique branch name with a key
+  ub["${branches[${i}]}"]=1
+done
+
+# checkout and pull each unique branch
+for b in ${!ub[@]} ; do
   git checkout "${b}"
   git pull
 done
+
+# switch back to original branch
 git checkout "${ob}"
