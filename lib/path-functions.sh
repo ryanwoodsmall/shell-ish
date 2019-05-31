d65e8845285f3891612f4e4770e8c2ab48b90652
ryan woodsmall <rwoodsmall@gmail.com>
2019-05-31 16:43:29 +0000

    lib/path-functions.sh: standardize formatting, quoting

--- lib/path-functions.sh
+++ lib/path-functions.sh
@@ -4,22 +4,22 @@
 
 # chomp trailing slash (if any)
 chomp_trailing_slash() {
-  echo ${@} | \
-    sed 's#/$##g'
+  echo "${@}" \
+  | sed 's#/$##g'
 }
 
 # is $1 in $PATH?
 in_path() {
-  echo ${PATH} | \
-    tr ':' '\n' | \
-    grep -q ^"`chomp_trailing_slash ${1}`"$
+  echo "${PATH}" \
+  | tr ':' '\n' \
+  | grep -q ^"`chomp_trailing_slash ${1}`"$
   return $?
 }
 
 # add $1 to end of $PATH
 append_path() {
-  in_path "${1}" || \
-    export PATH="${PATH}:${1}"
+  in_path "${1}" \
+  || export PATH="${PATH}:${1}"
 }
 
 
@@ -30,10 +30,10 @@ prepend_path() {
 
 # remove $1 from $PATH
 remove_path() {
-  in_path "${1}" && \
-    export PATH=$(echo ${PATH} \
-                  | tr ':' '\n' \
-                  | grep -v ^"`chomp_trailing_slash ${1}`"$ \
-                  | tr '\n' ':' \
-                  | sed 's#:$##g')
+  in_path "${1}" \
+  && export PATH=$(echo "${PATH}" \
+                   | tr ':' '\n' \
+                   | grep -v ^"`chomp_trailing_slash ${1}`"$ \
+                   | tr '\n' ':' \
+                   | sed 's#:$##g')
 }
