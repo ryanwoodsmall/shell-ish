dcffbdefd3df00500817ca1c75626dad7b50b226
ryan woodsmall <rwoodsmall@gmail.com>
2019-06-05 02:53:06 +0000

    docker-socat-docker-socket.sh: add socat image build

--- docker/docker-socat-docker-socket.sh
+++ docker/docker-socat-docker-socket.sh
@@ -4,12 +4,21 @@
 # serve up the docker socket to tcp://localhost:2375 via socat in a docker container
 #
 
+socat_image="socat"
+socat_name="socat_docker_socket"
+
+docker build \
+  --pull \
+  --force-rm \
+  --tag "${socat_image}" \
+    https://github.com/ryanwoodsmall/dockerfiles/raw/master/crosware/socat/Dockerfile
+
 docker run \
-  --name socat_docker_socket \
+  --name "${socat_name}" \
   --detach \
   --volume /var/run/docker.sock:/var/run/docker.sock \
   --network host \
   --privileged \
   --restart always \
-    socat \
+    "${socat_image}" \
       -d -d tcp4-listen:2375,fork,reuseaddr,bind=localhost unix-connect:/var/run/docker.sock
