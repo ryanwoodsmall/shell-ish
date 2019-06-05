c05b862dfcf29b9ef1408ad3c16f09384fb1dd44
ryan woodsmall <rwoodsmall@gmail.com>
2019-06-05 04:29:55 +0000

    docker-socat-tcp-listener.sh: fix socat_socket

--- docker/docker-socat-tcp-listener.sh
+++ docker/docker-socat-tcp-listener.sh
@@ -14,10 +14,10 @@
 set -eu
 
 : ${socat_port:="2375"}
-: ${socat_socketL:="/var/run/docker.sock"}
+: ${socat_socket:="/var/run/docker.sock"}
 
 socat \
   -v \
   -d -d \
   tcp4-listen:${socat_port},fork,reuseaddr \
-  unix-connect:/var/run/docker.sock
+  unix-connect:${socat_socket}
