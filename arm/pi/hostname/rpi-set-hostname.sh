b6df912cdca4a8ee49bbe6afe4c84d05d983c609
ryan woodsmall <rwoodsmall@gmail.com>
2019-06-21 00:55:10 +0000

    rpi-set-hostname.sh: only restart network if default hostname is set

--- arm/pi/hostname/rpi-set-hostname.sh
+++ arm/pi/hostname/rpi-set-hostname.sh
@@ -4,16 +4,23 @@ loopback="127.0.1.1"
 nic="eth0"
 hf="/etc/hosts"
 hnf="/etc/hostname"
+needrestart=0
 
 hnpref="$(xargs -0 echo < /proc/device-tree/model | tr A-Z a-z | sed 's/raspberry/r/g;s/model.*//g;s/ //g')"
 mac="$(cat /sys/class/net/${nic}/address | tr -d :)"
 
 newhostname="${hnpref}-${mac}"
 
+if $(grep -q '^raspberrypi$' ${hnf}) ; then
+  needrestart=1
+fi
+
 sed -i "/^${loopback} /d" ${hf}
-sed -i '/raspberrypi$/d' ${hf}
+sed -i '/^127.*raspberrypi$/d' ${hf}
 echo "${loopback} ${newhostname}" >> ${hf}
 echo ${newhostname} > ${hnf}
 
-hostname ${newhostname}
-systemctl restart networking.service
+if [ ${needrestart} -eq 1 ] ; then
+  hostname ${newhostname}
+  systemctl restart networking.service
+fi
