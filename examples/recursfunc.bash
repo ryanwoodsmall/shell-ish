81f2582cce8d68d05fc8f66ba5bbd5d4f30d443c
ryanwoodsmall <rwoodsmall@gmail.com>
2019-08-30 22:30:17 +0000

    examples/recursfunc.bash: add a "special" option checker/handler

--- examples/recursfunc.bash
+++ examples/recursfunc.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #
-# simple function that can output its own depth
+# simple function that can output its own depth...
+# and do something special every X levels deep
 #
 
 set -eu
@@ -9,11 +10,15 @@ set -eu
 function recurs() {
   local i="${1}"
   local s="${2}"
-  (( ${i} < ${s} )) && {
+  local x="${3}"
+  (( ${i} <= ${s} )) && {
     echo "call: ${i}, depth: ${#FUNCNAME[@]}"
-    #test $(( ${i} % 5 )) -eq 0 && special ${i}
-    recurs $(( ${i} + 1 )) ${s}
+    if [ ! ${x} -eq 0 ] ; then
+      test $(( ${i} % ${x} )) -eq 0 && special ${i}
+    fi
+    recurs $(( ${i} + 1 )) ${s} ${x}
   }
+  return 0
 }
 
 function special() {
@@ -22,14 +27,26 @@ function special() {
 }
 
 if [ ${#} -ne 0 ] ; then
+  e=0
   if [[ ${1} =~ ^[0-9]+$ ]] ; then
     n=${1}
   else
+    e=1
+  fi
+  if [ ${#} -gt 1 ] ; then
+    if [[ ${2} =~ ^[0-9]+$ ]] ; then
+      x=${2}
+    else
+      e=2
+    fi
+  fi
+  if [ ! ${e} -eq 0 ] ; then
     echo "please provide an integer argument" 1>&2
-    exit 1
+    exit ${e}
   fi
 fi
 
 : ${n:=10}
+: ${x:=0}
 
-recurs 1 ${n}
+recurs 1 ${n} ${x}
