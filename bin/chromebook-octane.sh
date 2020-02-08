d11e2ffe6b8400b0133b68bb2d8667cc4df855fe
ryanwoodsmall <rwoodsmall@gmail.com>
2020-02-08 10:23:03 +0000

    chromebook-octane.sh: add script to dump zipso chromebook scores from csv via curl+mlr

---
+++ bin/chromebook-octane.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for p in 'curl' 'mlr' ; do
+  if ! $(which ${p} >/dev/null 2>&1) ; then
+    echo "${p} not found"
+    exit 1
+  fi
+done
+
+curl -kLs https://zipso.net/chromebooks/output/chromebooks-all-zipso.csv \
+| sed 's#n/a#0#g;s#\*##g' \
+| mlr \
+    --icsvlite \
+    --opprint \
+    --barred \
+      cut -f 'Brand,Name,Screen,Resolution,Touchscreen,CPU model,CPU speed,Cores,Memory,storage,Octane' \
+      then \
+        sort -nr Octane \
+      then \
+        cat
