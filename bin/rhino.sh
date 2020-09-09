84c898c592964cacfdd632d47fc9a0074855906a
ryanwoodsmall <rwoodsmall@gmail.com>
2020-09-09 05:58:47 +0000

    bin/rhino.sh: update rhino to 1.7.13, move downloads to https

--- bin/rhino.sh
+++ bin/rhino.sh
@@ -2,14 +2,14 @@
 
 instdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-rhinover="1.7.12"
-rhinouri="http://central.maven.org/maven2/org/mozilla/rhino/${rhinover}/rhino-${rhinover}.jar"
+rhinover="1.7.13"
+rhinouri="https://repo1.maven.org/maven2/org/mozilla/rhino/${rhinover}/rhino-${rhinover}.jar"
 rhinofile="$(basename ${rhinouri})"
 rhinoclass="org.mozilla.javascript.tools.shell.Main"
-rhinosha256sum="9187b35bff89b74c3f2113abb64dbd62f2c63b2ac5da631fbd234e54318332d8"
+rhinosha256sum="931dda33789d8e004ff5b5478ee3d6d224305de330c48266df7c3e49d52fc606"
 
 jlinever="2.14.6"
-jlineuri="http://central.maven.org/maven2/jline/jline/${jlinever}/jline-${jlinever}.jar"
+jlineuri="https://repo1.maven.org/maven2/jline/jline/${jlinever}/jline-${jlinever}.jar"
 jlinefile="$(basename ${jlineuri})"
 jlinesha256sum="97d1acaac82409be42e622d7a54d3ae9d08517e8aefdea3d2ba9791150c2f02d"
 
