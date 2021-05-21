bcd3c629c2dd6f9c6ecde5e921e1dad36c28afd4
ryanwoodsmall <rwoodsmall@gmail.com>
2021-05-21 07:23:08 +0000

    examples/loopdetect.bash: notes, links

--- examples/loopdetect.bash
+++ examples/loopdetect.bash
@@ -2,6 +2,13 @@
 
 #
 # detect loops in function calls that can't be recursive
+# this currently checks that a function is calling itself and exits when a loop is detected
+# required functions are run in a loop and loops are detected only if the current req matches the function name
+# a more generalized form would check the stack before doing anything
+#
+# crosware uses something similar to detect recursive dependencies:
+#   https://github.com/ryanwoodsmall/crosware/blob/a9ad7c9044b2ee08c301f6f8175bdbdbc9db6660/bin/crosware#L1237-L1276
+#   https://github.com/ryanwoodsmall/crosware/blob/a9ad7c9044b2ee08c301f6f8175bdbdbc9db6660/recipes/common.sh#L117-L128
 #
 
 set -eu
