19016b9166327b3e35733b996651a4de07c2e77f
ryanwoodsmall <rwoodsmall@gmail.com>
2021-10-23 09:37:59 +0000

    bin/gopherweather.sh: add script

---
+++ bin/gopherweather.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# dump weather from floodgap gopher
+#
+set -eu
+
+: ${zip:="63105"}
+
+hash lynx >/dev/null 2>&1 || {
+  echo "$(basename ${BASH_SOURCE[0]}): 'lynx' not found" 1>&2
+  exit 1
+}
+
+lynx -dump "gopher://gopher.floodgap.com:70/7/groundhog/us/zipcode?${zip}"
