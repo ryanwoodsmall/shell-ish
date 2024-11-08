1ed35cbf6580266fb924bdd9181c4629bab6c8b7
ryanwoodsmall <rwoodsmall@gmail.com>
2024-11-08 02:46:46 -0600

    bin/gh-notifications.sh: add script

---
+++ bin/gh-notifications.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# XXX - flags only unread/disable seen
+# XXX - was gist: https://gist.github.com/ryanwoodsmall/45af848a73c017410bd8371816425321
+#
+
+for p in gh jq mlr ; do
+  command -v ${p} &>/dev/null || { echo "${p} not found, exiting" ; exit 1 ; }
+done
+
+{
+  echo 'name,version,url'
+  gh api --paginate /notifications \
+  | jq -Sr '.[]|["\(.repository.full_name)","\(.subject.title)","\(.subject.url)"]|@csv'
+} | mlr --icsv --opprint cat
