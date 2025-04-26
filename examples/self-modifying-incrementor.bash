d7261d7ec978351025f169cf9d637e3598118b13
ryanwoodsmall <rwoodsmall@gmail.com>
2025-04-25 21:00:29 -0500

    examples/self-modifying-incrementor.bash: serialize state to a variable

--- examples/self-modifying-incrementor.bash
+++ examples/self-modifying-incrementor.bash
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 #
 # self-modifying incrementor with like-named variable
-# unlike examples/self-modifying-counter.bash this takes optional starting and step values
-# exports a hash of YYYYMMDDHHMMSS to value
+# - unlike examples/self-modifying-counter.bash this takes optional starting and step values
+# - it exports a hash of YYYYMMDDHHMMSS to value
+# - it serializes its state to incr_s at every run
+#
 # XXX - only one second granularity, busybox date doesn't support real $N for nanosecond?
+# XXX - this could save its own state with a `define -f incr > incr.sh
+# XXX - saving incr_s to itself causes quick growth lol
+#       - printf 'export %s="%s"\n' "${funcname}_s" "$(eval echo \${${funcname}_s})"
 #
 
 declare -A | grep -q ' incr_h=' || declare -A incr_h=()
@@ -47,4 +52,10 @@ function incr() {
     fi
   done
   source /dev/stdin < <(for e in ${!funcarray[@]} ; do printf '%s\n' "${funcarray[${e}]}" ; done)
+  export ${funcname}_s=$({
+    printf 'export %s="%s"\n' "${funcname}" "${i}"
+    declare -A | grep -E "declare -A(|x) ${funcname}_h"
+    printf 'export %s\n' "${funcname}_h"
+    declare -f "${funcname}"
+  } | base64 | tr -d '\n')
 }
