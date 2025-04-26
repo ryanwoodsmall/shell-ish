01d2bd65f896b42a98924ecd5d66ba3d18c4bb4c
ryanwoodsmall <rwoodsmall@gmail.com>
2025-04-25 19:29:12 -0500

    examples/self-modifying-incrementor.bash: add one second granularity (???) time->value incr_h hash

--- examples/self-modifying-incrementor.bash
+++ examples/self-modifying-incrementor.bash
@@ -2,7 +2,12 @@
 #
 # self-modifying incrementor with like-named variable
 # unlike examples/self-modifying-iterator.bash this takes optional starting and step values
+# exports a hash of YYYYMMDDHHMMSS to value
+# XXX - only one second granularity, busybox date doesn't support real $N for nanosecond?
 #
+
+declare -A | grep -q ' incr_h=' || declare -A incr_h=()
+export incr_h
 
 function incr() {
   local -i i=0
@@ -10,6 +15,7 @@ function incr() {
   local -i n=0
   local funcname="${FUNCNAME[0]}"
   local -a funcarray
+  local ts="$(date +%Y%m%d%H%M%S)"
   readarray -t funcarray < <(declare -f "${funcname}")
   funcarray[0]="function ${funcarray[0]}"
   if [[ ${#} -gt 0 ]] ; then
@@ -30,6 +36,8 @@ function incr() {
   fi
   n=$((i+p))
   export ${funcname}="${i}"
+  eval "${funcname}_h[\"${ts}\"]=\"${i}\""
+  export ${funcname}_h
   printf '%s\n' "${i}"
   for e in ${!funcarray[@]} ; do
     if [[ "${funcarray[${e}]}" =~ "local -i i=" ]] && [[ "${funcarray[${e}]}" =~ i=(|-)[0-9]+ ]] ; then
